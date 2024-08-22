64a83eb6ee9ad47532f6e0c0fcba1e6b3a25b14d
Wiiseguy <wiiseguy@gmail.com>
2024-08-22 19:06:21 +0200

    Moved money p/s counter to top

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,31 @@
-const COUNTER_SAMPLE_LENGTH = 10
+const COUNTER_SAMPLE_LENGTH = 5
 
 export class Counter {
     constructor(name, fn) {
         this.name = name
         this.fn = fn
         this.delta = 0
+        this.smoothedDelta = 0
         this.prevValues = []
+        this.prevDeltas = []
     }
     update() {
         this.prevValues.push(this.fn())
         if (this.prevValues.length > COUNTER_SAMPLE_LENGTH) {
             this.prevValues.shift()
+        }
 
-            let deltas = []
-            for (let i = 1; i < this.prevValues.length; i++) {
-                deltas.push(this.prevValues[i] - this.prevValues[i - 1])
-            }
-            this.delta = deltas.reduce((acc, val) => acc + val, 0) / deltas.length
+        let deltas = []
+        for (let i = 1; i < this.prevValues.length; i++) {
+            deltas.push(this.prevValues[i] - this.prevValues[i - 1])
+        }
+        this.delta = deltas.reduce((acc, val) => acc + val, 0) / (deltas.length || 1)
+        this.prevDeltas.push(this.delta)
+
+        if (this.prevDeltas.length > COUNTER_SAMPLE_LENGTH) {
+            this.prevDeltas.shift()
+            this.smoothedDelta = this.prevDeltas.reduce((acc, val) => acc + val, 0) / (this.prevDeltas.length || 1)
+            this.delta = this.smoothedDelta
         }
     }
 }
